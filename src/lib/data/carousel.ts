b708658c397a1e41fea1dcadb6387187f816dd89
Chris Ross <chrislandonross@gmail.com>
2024-04-07 14:37:46 +0000

    Start from scratch using Svelte this time

---
+++ src/lib/data/carousel.ts
@@ -0,0 +1,10 @@
+export const images = [
+    {
+        alt: "Kubernetes",
+        src: "/logos/kubernetes_logo.svg"
+    },
+    {
+        alt: "Docker",
+        src: "/logos/docker_logo.svg"
+    }
+];
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import "../app.pcss";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  import { Footer, FooterCopyright } from 'flowbite-svelte';
+
+  const year: number = new Date().getFullYear();
+  import { page } from '$app/stores';
+  $: activeUrl = $page.url.pathname;
+</script>
+
+<div class="container mx-auto max-w-5xl min-h-screen flex flex-col justify-between items-center">
+  <Navbar>
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Chris Ross</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl {activeUrl}>
+      <NavLi href="/experience">Experience</NavLi>
+      <NavLi href="/projects">Projects</NavLi>
+      <NavLi href="/cv">CV</NavLi>
+    </NavUl>
+  </Navbar>
+
+  <div>
+    <slot />
+  </div>
+
+  <Footer class="p-7">
+    <FooterCopyright href="/" by="Chris Ross" year={year} />
+  </Footer>
+</div>
