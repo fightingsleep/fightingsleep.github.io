b0b726d5e29f2acd296c782d258f86548fc84fb5
Chris Ross <chrislandonross@gmail.com>
2024-04-07 14:37:46 +0000

    Start from scratch using Svelte this time

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+import { images } from '$lib/data/carousel';
+import { Carousel, Heading, P } from 'flowbite-svelte';
+import { scale } from 'svelte/transition';
+import { quintOut } from 'svelte/easing';
+
+const scaleAnimation = (x) => scale(x, { duration: 500, easing: quintOut });
+</script>
+
+<div>
+    <Heading tag="h1" class="mb-4 text-center" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl">Chris Ross</Heading>
+    <P class="text-center mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Senior Software Developer</P>
+    <Carousel {images} transition={scaleAnimation} class="justify-items-center max-h-24" imgClass="object-contain h-full max-w-28 rounded-sm p-2" duration={3000} />
+</div>
+
+<style>
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { Card, Button } from 'flowbite-svelte';
+    import { ArrowRightOutline } from 'flowbite-svelte-icons';
+</script>
+
+<div>
+  <div class="grid grid-cols-2 gap-4">
+    <Card img="/images/image-1.webp">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <Button>
+        Read more <ArrowRightOutline class="w-3.5 h-3.5 ms-2 text-white" />
+      </Button>
+    </Card>
+    <Card img="/images/image-1.webp">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <Button>
+        Read more <ArrowRightOutline class="w-3.5 h-3.5 ms-2 text-white" />
+      </Button>
+    </Card>
+    <Card img="/images/image-1.webp">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <Button>
+        Read more <ArrowRightOutline class="w-3.5 h-3.5 ms-2 text-white" />
+      </Button>
+    </Card>
+    <Card img="/images/image-1.webp">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <Button>
+        Read more <ArrowRightOutline class="w-3.5 h-3.5 ms-2 text-white" />
+      </Button>
+    </Card>
+  </div>
+</div>
+
+<style lang="scss">
+  // Import any necessary stylesheets or libraries here
+
+  // Write your component's styles using SCSS and Tailwind CSS here
+</style>
