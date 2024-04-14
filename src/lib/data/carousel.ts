b6959b6fc682ae991173257fd055b037f890d5a4
Chris Ross <chrislandonross@gmail.com>
2024-04-14 16:43:40 +0100

    Add base path to data files

--- src/lib/data/carousel.ts
+++ src/lib/data/carousel.ts
@@ -1,26 +1,28 @@
+import { base } from '$app/paths';
+
 export const images = [
   {
     alt: "Kubernetes",
-    src: "/logos/kubernetes_logo.svg"
+    src: `${base}/logos/kubernetes_logo.svg`
   },
   {
     alt: "Docker",
-    src: "/logos/docker_logo.svg"
+    src: `${base}/logos/docker_logo.svg`
   },
   {
     alt: "Python",
-    src: "/logos/python_logo.svg"
+    src: `${base}/logos/python_logo.svg`
   },
   {
     alt: "C#",
-    src: "/logos/csharp_logo.svg"
+    src: `${base}/logos/csharp_logo.svg`
   },
   {
     alt: "C++",
-    src: "/logos/cpp_logo.svg"
+    src: `${base}/logos/cpp_logo.svg`
   },
   {
     alt: "Argo Workflows",
-    src: "/logos/argo_logo.svg"
+    src: `${base}/logos/argo_logo.svg`
   }
 ];
